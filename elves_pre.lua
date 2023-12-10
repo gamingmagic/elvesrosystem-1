d8e77806b05c2fc1ff85a9ee99c51700af862293
raffles <53240858+holic-cat@users.noreply.github.com>
2023-12-10 13:59:55 +0800

    Update elves_pre.lua

--- elves_pre.lua
+++ elves_pre.lua
@@ -91067,10 +91067,10 @@ tbl = {
 		identifiedDisplayName = "Elves Hardrock Mammoth Card",
 		identifiedResourceName = "이름없는카드",
 		identifiedDescriptionName = {
-			"When equipped with ^990099 Edgga Card^000000",
-			"Increases damage of ^009900Cart Termination^000000 by 30%.",
+			"Reduce Reflect Damage by Large Percentage to Swordman and Merchant Class",
+			"Increase 10% More HP to Merchant Class",
 			"When equipped with ^990099 Elves Edgga Card^000000",
-			"Increases damage of ^009900Cart Termination^000000 by 50%.",
+			"Increases damage of ^009900Cart Termination^000000 by 55%.",
 			"Compound on:^00CC33 Accessory^000000",
 		},
 		slotCount = 0,
